200470bbbe439be0588eab58f8e524029f6da186
AustinWithoutPowers <austinrudkin@live.com>
2025-11-30 22:53:27 +1300

    Created base functions and tests for Suit and Number classes

--- src/classes.py
+++ src/classes.py
@@ -1,3 +1,87 @@
-class Suit: ...
-class Number: ...
+class Suit:
+    CLUB: str = 'c'
+    DIAMOND: str = 'd'
+    HEART: str = 'h'
+    SPADE: str = 's'
+    _SUITS: list[str] = [CLUB, DIAMOND, HEART, SPADE]
+
+    def __init__(self, suit_str: str) -> None:
+        self._suit_str: str = ''
+        self._add_suit_str(suit_str=suit_str)
+    
+    # test: test_suit_str_invalid
+    def _add_suit_str(self, suit_str: str) -> None:
+        if suit_str not in self._SUITS:
+            raise ValueError(f'ERROR: SUITSTRINVALID\nSuit string {suit_str}' + \
+                             f' not in {self._SUITS}.')
+        self._suit_str = suit_str
+    
+    # test: test_suit_getter
+    @property
+    def suit_str(self) -> str:
+        return self._suit_str
+    # test: test_suit_setter
+    @suit_str.setter
+    def suit_str(self, suit_str: str) -> None:
+        self._add_suit_str(suit_str=suit_str)
+    
+    # test: test_suit_eq
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Suit):
+            return NotImplemented
+        return self.suit_str == other.suit_str
+    
+    # test: test_suit_ne
+    def __ne__(self, other: object) -> bool:
+        if not isinstance(other, Suit):
+            return NotImplemented
+        return not self == other
+
+class Number:
+    ACE: int = 1
+    JACK: int = 11
+    QUEEN: int = 12
+    KING: int = 13
+    ACE2: int = 14
+    ACES: list[int] = [ACE, ACE2]
+    _NUMBERS: list[int] = [ACE, 2, 3, 4, 5, 6, 7, 8,\
+                           9, 10, JACK, QUEEN, KING, ACE2]
+    
+    def __init__(self, number: int) -> None:
+        self._number: int = 0
+        self._add_number(number=number)
+    
+    # test: test_number_invalid
+    def _add_number(self, number: int) -> None:
+        if number not in self._NUMBERS:
+            raise ValueError(f'ERROR: NUMBERINVALID\nNumber {number}' + \
+                             f' not in {self._NUMBERS}.')
+        self._number = number
+    
+    # test: test_number_getter
+    @property
+    def number(self) -> int:
+        return self._number
+    # test: test_suit_setter
+    @number.setter
+    def number(self, number: int) -> None:
+        self._add_number(number=number)
+    
+    # test: test_number_eq, test_number_eq_ace_one, test_number_eq_ace_two,
+    #       test_number_eq_ace_dif
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Number):
+            return NotImplemented
+        if self.number in self.ACES:
+            return other.number in other.ACES
+        return self.number == other.number
+    
+    # test: test_number_ne, test_number_ne_ace_first, test_number_ne_ace_second
+    def __ne__(self, other: object) -> bool:
+        if not isinstance(other, Number):
+            return NotImplemented
+        return not self == other
+
 class Card: ...
+class Hand: ...
+class CommunityCards: ...
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,4 +1,74 @@
-from src.classes import Suit
+import unittest
+from src.classes import Suit, Number
 
-Suit()
-print('Unit tests successful!')
+class SuitTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_suit = Suit(suit_str=Suit.CLUB)
+    
+    def test_suit_str_invalid(self):
+        with self.assertRaises(Exception) as context:
+            _ = Suit('p')
+        self.assertTrue('ERROR: SUITSTRINVALID' in str(context.exception))
+    
+    def test_suit_getter(self):
+        self.assertEqual(self.test_suit.suit_str, Suit.CLUB)
+    
+    def test_suit_setter(self):
+        test_suit = Suit(suit_str=Suit.CLUB)
+        test_suit.suit_str = Suit.DIAMOND
+        self.assertEqual(test_suit.suit_str, Suit.DIAMOND)
+    
+    def test_suit_eq(self):
+        self.assertTrue(self.test_suit == Suit(Suit.CLUB))
+
+    def test_suit_ne(self):
+        self.assertTrue(self.test_suit != Suit(Suit.DIAMOND))
+
+class NumberTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_number = Number(number=2)
+    
+    def test_number_invalid(self):
+        with self.assertRaises(Exception) as context:
+            _ = Number(29)
+        self.assertTrue('ERROR: NUMBERINVALID' in str(context.exception))
+    
+    def test_number_getter(self):
+        self.assertEqual(self.test_number.number, 2)
+    
+    def test_number_setter(self):
+        test_number = Number(number=Number.ACE)
+        test_number.number = 2
+        self.assertEqual(test_number.number, 2)
+    
+    def test_number_eq(self):
+        self.assertTrue(self.test_number == Number(2))
+    
+    def test_number_eq_ace_one(self):
+        test_ace = Number(number=Number.ACE)
+        test_another_ace = Number(number=Number.ACE)
+        self.assertTrue(test_ace == test_another_ace)
+    
+    def test_number_eq_ace_two(self):
+        test_ace = Number(number=Number.ACE2)
+        test_another_ace = Number(number=Number.ACE2)
+        self.assertTrue(test_ace == test_another_ace)
+    
+    def test_number_eq_ace_dif(self):
+        test_ace = Number(number=Number.ACE)
+        test_another_ace = Number(number=Number.ACE2)
+        self.assertTrue(test_ace == test_another_ace)
+
+    def test_number_ne(self):
+        self.assertTrue(self.test_number != Number(3))
+    
+    def test_number_ne_ace_first(self):
+        test_ace = Number(number=Number.ACE)
+        self.assertTrue(test_ace != self.test_number)
+    
+    def test_number_ne_ace_second(self):
+        test_ace = Number(number=Number.ACE)
+        self.assertTrue(self.test_number != test_ace)
+
+if __name__ == '__main__':
+    unittest.main()
